02b8f7e3fb9f19e2ab589f8b8ae67b135971d53b
LEE-PC\car89 <car8953@gmail.com>
2024-01-31 16:05:19 +0900

    최초

---
+++ RestTemplate-Demo/README.md
@@ -0,0 +1,12 @@
+# Spring RestTemplate Demo
+- Method : Get, Post, Put, Patch, Delete
+- 실행 : RestTemplateTest.java 참조
+
+## 설명
+- RestTemplate을 사용한 Http 통신 구현
+- 응답 클래스 객체로 변환 가능
+- RestTemplate Interceptor를 사용한 로깅 개발완료 (method, uri, header, body)
+
+## Branch
+- v1 : new 키워드로 restTemplate 통신 기능완료
+- v2 : v1을 수정하여 restTemplate을 Bean으로 관리, 컴포넌트화 완료
---
+++ RestTemplate-Demo/src/test/java/kr/rojae/rest/RestTemplateTest.java
@@ -0,0 +1,87 @@
+package kr.kitech.rest;
+
+import kr.rojae.rest.http.HttpHeader;
+import kr.rojae.rest.http.RestProvider;
+import kr.rojae.rest.model.Comment;
+import kr.rojae.rest.model.Person;
+import kr.rojae.rest.model.PersonEnrollResponse;
+import kr.rojae.rest.props.ApiProp;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@SpringBootTest
+@Slf4j
+public class RestTemplateTest {
+
+    @Autowired
+    private RestProvider restProvider;
+
+    @Autowired
+    private ApiProp apiProp;
+
+    @Test
+    public void getTest1(){
+        ResponseEntity<Person[]> response = restProvider.send(HttpMethod.GET, apiProp.getUrl1, Person[].class);
+        Person[] persons = response.getBody();
+        assert persons != null;
+        Arrays.stream(persons).forEach(p -> log.info(
+            p.toString()
+        ));
+    }
+
+    @Test
+    public void getTest2(){
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("postId", Collections.singletonList("1"));
+
+        ResponseEntity<Comment[]> response = restProvider.send(HttpMethod.GET, apiProp.getUrl2, params, new HttpHeader(MediaType.APPLICATION_JSON), Comment[].class);
+        Comment[] comments = response.getBody();
+        assert comments != null;
+        Arrays.stream(comments).forEach(c -> log.info(
+                c.toString()
+        ));
+    }
+
+    @Test
+    public void postTest1(){
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("postId", Collections.singletonList("bodyValue..."));
+
+        ResponseEntity<PersonEnrollResponse> response = restProvider.send(HttpMethod.POST, apiProp.postUrl, params, new HttpHeader(MediaType.APPLICATION_JSON), PersonEnrollResponse.class);
+        PersonEnrollResponse data = response.getBody();
+        assert data != null;
+        log.info(data.toString());
+    }
+
+    @Test
+    public void putTest1(){
+        ResponseEntity<PersonEnrollResponse> response = restProvider.send(HttpMethod.PUT, apiProp.putUrl, new HttpHeader(MediaType.APPLICATION_JSON), PersonEnrollResponse.class);
+        PersonEnrollResponse data = response.getBody();
+        assert data != null;
+        log.info(data.toString());
+    }
+
+    @Test
+    public void patchTest1(){
+        ResponseEntity<Person> response = restProvider.send(HttpMethod.PATCH, apiProp.patchUrl, new HttpHeader(MediaType.APPLICATION_JSON), Person.class);
+        Person data = response.getBody();
+        assert data != null;
+        log.info(data.toString());
+    }
+
+    @Test
+    public void deleteTest1(){
+        ResponseEntity<PersonEnrollResponse> response = restProvider.send(HttpMethod.DELETE, apiProp.deleteUrl, new HttpHeader(MediaType.APPLICATION_JSON), PersonEnrollResponse.class);
+    }
+
+}
